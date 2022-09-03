17438b64b182a7d0112fb8c95ccb7c5b05c44ee2
Vifeng <virgvm@gmail.com>
2022-09-03 20:28:40 +0200

    User Inputs in Vue

--- my-vue-app/src/components/HelloWorld.vue
+++ my-vue-app/src/components/HelloWorld.vue
@@ -8,11 +8,19 @@
     {id: 2, label: "cake"},
     {id: 3, label: "bunny"}
   ])
+  const newItem = ref("")
+  const newItemHighPriority = ref(false)
   </script>
   
   <template>
     <h1>{{ header }}</h1>
-    <input v-model="header"/>
+    <input v-model="newItem" placeholder="Add Item"/>
+    <label>
+      <input v-model="newItemHighPriority" type="checkbox"/>
+      High Priority
+    </label>
+    <br>
+    {{newItemHighPriority}}
     <ul>
       <li v-for="item in items" :key="item.id">
         {{item.label}}
--- my-vue-app/src/components/Memos/HelloWorldMemo.vue
+++ my-vue-app/src/components/Memos/HelloWorldMemo.vue
@@ -13,16 +13,50 @@
       'item-2':{id: 2, label: "cake"},
       'item-3':{id: 3, label: "bunny"}
     })
+    const newItemPriority = ref("low")
+    const IceCreamFlavors = ref([
+    ])
+
     </script>
     
     <template>
       <h1>{{ txt.toLocaleUpperCase() || header }}</h1>
       <h2>{{ txt ? txt : 'bouh' }}</h2>
-      <input v-model="header"/>
+      <div class="memo">
+      IceCreamFlavors:
+      <label>
+        <input type="checkbox" v-model="IceCreamFlavors" value="chocolate"/>Choco
+      </label>
+      <label>
+        <input type="checkbox" v-model="IceCreamFlavors" value="coffee"/>Coffee
+      </label>
+      <label>
+        <input type="checkbox" v-model="IceCreamFlavors" value="pear"/>Pear
+      </label>
+      <br>
+      Priority:
+    <label>
+      <input v-model="newItemPriority" type="radio" value="low"/>Low Priority
+      <input v-model="newItemPriority" type="radio" value="high"/>High Priority
+    </label>
+    <label>
+      <select v-model="newItemPriority">
+        <option value="low">Low</option>
+        <option value="high">High</option>
+      </select>
+    </label>
+<br>
+    {{newItemPriority}}
       <ul>
       <li v-for="({id, label}, index) in itemsArray" :key="id">
         {{index}} - {{label}}</li>
       <li v-for="({id, label}, index) in itemsObject" :key="id">
         {{index}} - {{label}}</li>
     </ul>
+  </div>
     </template>
+
+    <style>
+      @import './mainMemo.css'
+
+    </style>
