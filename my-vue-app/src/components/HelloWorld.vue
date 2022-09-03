60558cd0ae0774469e5dfeb5fa3a3002141de667
Vifeng <virgvm@gmail.com>
2022-09-03 19:36:20 +0200

    List Rendering in Vue

--- my-vue-app/src/components/HelloWorld.vue
+++ my-vue-app/src/components/HelloWorld.vue
@@ -3,11 +3,22 @@
   import { ref } from 'vue'
   
   const header = ref('Shopping List App')
+  const items = ref([
+    {id: 1, label: "carrots"},
+    {id: 2, label: "cake"},
+    {id: 3, label: "bunny"}
+  ])
   </script>
   
   <template>
     <h1>{{ header }}</h1>
     <input v-model="header"/>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{item.label}}
+      </li>
+    </ul>
+
   </template>
 
   <style>
--- my-vue-app/src/components/Memos/HelloWorldMemo.vue
+++ my-vue-app/src/components/Memos/HelloWorldMemo.vue
@@ -3,11 +3,26 @@
     // import {msg} from 'vue'
     const header = ref('Component 2 : Memo')
     const txt = ref('')
+    const itemsArray = ref([
+    {id: 1, label: "carrots"},
+    {id: 2, label: "cake"},
+    {id: 3, label: "bunny"}
+  ])
+    const itemsObject = ref({
+      'item-1':{id: 1, label: "carrots"},
+      'item-2':{id: 2, label: "cake"},
+      'item-3':{id: 3, label: "bunny"}
+    })
     </script>
     
     <template>
       <h1>{{ txt.toLocaleUpperCase() || header }}</h1>
       <h2>{{ txt ? txt : 'bouh' }}</h2>
-     <h3 >{{msg}} xxo</h3>
-     <input v-model="header"/>
+      <input v-model="header"/>
+      <ul>
+      <li v-for="({id, label}, index) in itemsArray" :key="id">
+        {{index}} - {{label}}</li>
+      <li v-for="({id, label}, index) in itemsObject" :key="id">
+        {{index}} - {{label}}</li>
+    </ul>
     </template>
