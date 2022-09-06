5e06a7dfe345c33942677dead32f1b74bf718cf0
Vifeng <virgvm@gmail.com>
2022-09-06 17:31:27 +0200

    end of course

---
+++ my-vue-app/src/components/Myreactive.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import {ref, reactive} from 'vue'
+
+    // see https://vuejs.org/guide/extras/reactivity-in-depth.html#computed-debugging
+
+    // don't work with primitives reactive({0}), reactive({true}), reactive({'hello'})
+    // but can live in sth big such as count here
+    const state = reactive({count: 0})
+    // work without def
+    const increment = ()=>{
+        state.count ++
+    }
+    
+    const msg = ref('hello Wolrd!')
+</script>
+
+
+<template>
+    <h1> {{state.count}}</h1>
+    <button 
+        @click="increment">
+        Increment count
+    </button>
+</template>
